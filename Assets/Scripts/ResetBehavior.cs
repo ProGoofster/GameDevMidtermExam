55a327e88b99837bf70889d06aceb04f32f29164
Jake Berryman <dragons4me@comcast.net>
2025-02-28 16:40:27 -0600

    begin work on reset butotn

---
+++ Assets/Scripts/ResetBehavior.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ResetBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Reset(){
+        
+    }
+}
